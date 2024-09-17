94c793fadda5834b27c02f2114e0e0f8ac8fbbc3
Alex <bokanchaalexandra@gmail.com>
2024-09-17 17:18:09 +0200

    ex00 finished

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -5,13 +5,26 @@
 class ClapTrap
 {
 	private:
+
+        std::string     _name;
+        unsigned int    _hitPoints;
+        unsigned int    _energyPoints;
+        unsigned int    _attackDamage;
 		
     public:
-        ClapTrap(void);
-        ClapTrap(const ClapTrap& other);
-        ClapTrap &operator=(const ClapTrap &other);
+
+        ClapTrap( void );
+        ClapTrap( std::string name );
+        ClapTrap( const ClapTrap & rhs );
+        ClapTrap &operator=( const ClapTrap & rhs );
         ~ClapTrap();
+
+        void attack( const std::string& target );
+        void takeDamage( unsigned int amount );
+        void beRepaired( unsigned int amount );
 };
+
+std::ostream & operator<<(std::ostream& o, ClapTrap const & newFixed);
 
 #endif
 
